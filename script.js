4bb9aaceac6b0dcc019a1ce5e3991f291061c58e
SubatYucel <subat.yucel1@gmail.com>
2024-05-29 18:49:27 +0300

    everything is done

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggle() {
   var modal = document.getElementById("modal-window");
   modal.classList.toggle("is-open");
 }
+
+function toggleMenu() {
+  var menu = document.getElementById("mobile-toggle-menu");
+  menu.classList.toggle("is-open");
+}
